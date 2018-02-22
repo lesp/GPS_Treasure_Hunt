33282f38ca75ecfc005d0fc462de96f64be69b61
Les Pounder <lespounder@gmail.com>
2018-02-22 19:59:37 +0000

    Working code

--- mapper.py
+++ mapper.py
@@ -11,4 +11,4 @@ longitudes = [Tower[1],Zoo[1],Amusement_Park[1]]
 gmap = gmplot.GoogleMapPlotter(53.812972, -3.024459, 15)
 gmap.scatter(latitudes, longitudes, color = choice(colours) , marker=True)
 gmap.draw("mymap.html")
-webbrowser.open("mymap.html", new=0)
+webbrowser.open("frame.html", new=0)
