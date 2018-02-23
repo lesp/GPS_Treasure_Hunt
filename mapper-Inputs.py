dcd7ffb6282bc09a2ad859a4edfd4aea9c6867bf
Les Pounder <lespounder@gmail.com>
2018-02-23 14:11:45 +0000

    Now shows user position

--- mapper-Inputs.py
+++ mapper-Inputs.py
@@ -1,4 +1,5 @@
 import gmplot, webbrowser, gpsd, time, subprocess
+#Call cgps to ensure that we have a stable GPS signal
 subprocess.call(["cgps"," &"])
 time.sleep(3)
 gpsd.connect()
@@ -12,12 +13,18 @@ longitudes = [Tower[1],Zoo[1],Amusement_Park[1]]
 #Set current position
 packet = gpsd.get_current()
 print(packet.position())
+#Create two lists, for the location lat & long, as heatmap needs an iterable object.
 loc_lat = [packet.position()[0]]
 loc_long = [packet.position()[1]]
+#Centre the map on the user
 gmap = gmplot.GoogleMapPlotter(packet.position()[0], packet.position()[1], 20)
+#Plot the user position using a heatmap
 gmap.heatmap(loc_lat,loc_long)
+#Plot where the treasures
 gmap.scatter(latitudes, longitudes, 'r', marker=True)
+#Draw the map
 gmap.draw("mymap.html")
+#Open a web browser
 webbrowser.open("frame.html", new=0)
 
 while True:
