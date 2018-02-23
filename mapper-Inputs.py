f3643a4ce41c093931d739e0632bfefb7ca2e0c5
Les Pounder <lespounder@gmail.com>
2018-02-23 12:11:02 +0000

    Paranoia based backup

---
+++ mapper-Inputs.py
@@ -0,0 +1,24 @@
+import gmplot, webbrowser, gpsd, time
+gpsd.connect()
+#Locations to find
+Tower = (53.8159877,-3.0554085)
+Zoo = (53.8157874,-3.0107095)
+Amusement_Park = (53.7924182,-3.0556338)
+#Add them to a list
+latitudes = [Tower[0],Zoo[0],Amusement_Park[0]]
+longitudes = [Tower[1],Zoo[1],Amusement_Park[1]]
+#Set current position
+packet = gpsd.get_current()
+print(packet.position())
+gmap = gmplot.GoogleMapPlotter(packet.position()[0], packet.position()[1], 20)
+gmap.heatmap(packet.position()[0], packet.position()[1])
+gmap.scatter(latitudes, longitudes, 'r', marker=True)
+gmap.draw("mymap.html")
+webbrowser.open("frame.html", new=0)
+
+while True:
+    packet = gpsd.get_current()
+    print(packet.position())
+    gmap = gmplot.GoogleMapPlotter(packet.position()[0], packet.position()[1], 20)
+    gmap.heatmap(packet.position()[0], packet.position()[1])
+    time.sleep(5)
